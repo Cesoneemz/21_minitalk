211be988b1ed135126146bc044cf6285df67f605
wlanette <wlanette@student.21-school.ru>
2021-12-15 14:43:54 +0300

    Add usleep in client

--- srcs/main_client.c
+++ srcs/main_client.c
@@ -6,7 +6,7 @@
 /*   By: wlanette <wlanette@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 12:00:17 by wlanette          #+#    #+#             */
-/*   Updated: 2021/12/15 12:24:10 by wlanette         ###   ########.fr       */
+/*   Updated: 2021/12/15 14:32:21 by wlanette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,12 +48,10 @@ static int	ft_send_message(int pid, char *str)
 	if (message[++bits / 8])
 	{
 		if (message[bits / 8] & (0x80 >> (bits % 8)))
-		{
-			if (kill(save_pid, SIGUSR1) == -1)
-				ft_print_error(message);
-		}
-		else if (kill(save_pid, SIGUSR2) == -1)
-			ft_print_error(message);
+			kill(save_pid, SIGUSR1);
+		else
+			kill(save_pid, SIGUSR2);
+		usleep(300);
 		return (0);
 	}
 	if (!ft_send_null(save_pid, message))
